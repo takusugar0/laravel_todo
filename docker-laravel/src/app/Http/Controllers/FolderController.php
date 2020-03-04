222ee5d7c656239c871b02103a9c9a5a1cf6ca71
takusugar0 <takusugar9@gmail.com>
2020-03-04 15:16:45 +0900

    first-commit

---
+++ docker-laravel/src/app/Http/Controllers/FolderController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Requests\CreateFolder;
+use App\Folder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FolderController extends Controller
+{
+    public function showCreateForm()
+    {
+        return view('folders/create');
+    }
+    
+    public function create(CreateFolder $request)
+    {
+        // フォルダモデルのインスタンスを作成する
+        $folder = new Folder();
+        // タイトルに入力値を代入する
+        $folder->title = $request->title;
+
+        // ユーザーに紐づけて保存
+        Auth::user()->folders()->save($folder);
+
+        // // インスタンスの状態をデータベースに書き込む
+        // $folder->save();
+    
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
+    }
+}
---
+++ docker-laravel/src/app/Http/Controllers/TaskController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Folder;
+use App\Task;
+use App\Http\Requests\CreateTask;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    public function index(int $id)
+    {
+        // // すべてのフォルダを取得する
+        // $folders = Folder::all();
+
+         // ユーザーのフォルダを取得する
+         $folders = Auth::user()->folders()->get();
+    
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+    
+        // 選ばれたフォルダに紐づくタスクを取得する
+        $tasks = $current_folder->tasks()->get();
+        // $tasks = Tasks::where('folder_id', $current_folder->id)->get();
+    
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+　　 * GET /folders/{id}/tasks/create
+　　 */
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+        'folder_id' => $id
+        ]);
+    }
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+    
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+    
+        $current_folder->tasks()->save($task);
+    
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
+        ]);
+    }
+
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+    
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
+
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+        // 1
+        $task = Task::find($task_id);
+    
+        // 2
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+    
+        // 3
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
+        ]);
+    }
+
+}
